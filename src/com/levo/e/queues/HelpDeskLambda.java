25bf600ee2c047a95386e1223af164e032be8c2c
Levent Divilioglu <divilioglu@gmail.com>
2016-06-30 01:38:01 +0300

    queues

--- src/com/levo/e/queues/HelpDeskLambda.java
+++ src/com/levo/e/queues/HelpDeskLambda.java
@@ -2,8 +2,9 @@ package com.levo.e.queues;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.function.Predicate;
 
-public class HelpDesk {
+public class HelpDeskLambda {
 	
 	private final Queue<Enquiry> enquiries = new ArrayDeque<>();
 	
@@ -12,27 +13,29 @@ public class HelpDesk {
 	}
 	
 	public void processPrinterEnquiry() {
-		final Enquiry enquiry = enquiries.peek();
-		if(enquiry != null && enquiry.getCategory() == Category.PRINTER) {
-			enquiries.remove();
-			enquiry.getCustomer().reply("Is it out of paper?");
-		} else {
-			System.out.println("No work to do, let's have some coffee!");
-		}
+		processEnquiry(
+				enq -> enq.getCategory() == Category.PRINTER,
+				"Is it out of paper?");
 	}
 	
 	public void processGeneralEnquiry() {
+		processEnquiry(
+				enq -> enq.getCategory() != Category.PRINTER,
+				"Have you tried turning it off and on again?");
+	}
+
+	private void processEnquiry(Predicate<Enquiry> predicate, final String message) {
 		final Enquiry enquiry = enquiries.peek();
-		if(enquiry != null && enquiry.getCategory() != Category.PRINTER) {
+		if(enquiry != null && predicate.test(enquiry)) {
 			enquiries.remove();
-			enquiry.getCustomer().reply("Have you tried turning it off and on again?");
+			enquiry.getCustomer().reply(message);
 		} else {
 			System.out.println("No work to do, let's have some coffee!");
 		}
 	}
 	
 	public static void main(String[] args) {
-		HelpDesk helpDesk = new HelpDesk();
+		HelpDeskLambda helpDesk = new HelpDeskLambda();
 		
 		helpDesk.enquire(Customer.JACK, Category.PHONE);
 		helpDesk.enquire(Customer.JILL, Category.PRINTER);
